a13c2ecd90cee078e6537fd691832492d9994832
FieryInferno <bagassetia271@gmail.com>
2021-10-20 09:41:41 +0700

    Mengubah rusak telat menjadi telat

--- resources/views/petugas/v_entri-pelanggaran-baru.blade.php
+++ resources/views/petugas/v_entri-pelanggaran-baru.blade.php
@@ -2,83 +2,79 @@
 @section('title', 'Entri Koleksi')
 @section('content')
 <section class="content-header">
-
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1>Entri Data Pelanggaran</h1>
-            </div>
-        </div>
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1>Entri Data Pelanggaran</h1>
+      </div>
     </div>
+  </div>
 </section>
 
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card card-primary card-outline">
-                <div class="card-body">
-                    <div class="col-md-12">
-                        <form action="{{url('/simpan-pelanggaran')}}" method="POST">
-                            @csrf
-
-                            <input type="text" name="no_transaksi" value="{{$pelanggaran->no_transaksi}}" hidden>
-                            <input type="text" name="id_buku" value="{{$pelanggaran->id_buku}}" hidden>
-
-
-                            <div class="form-group row">
-                                <label class="col-md-2 col-form-label text-md-right">Jenis Pelanggaran</label>
-                                <div class="col-md-6">
-                                    <select class="custom-select" name="jenis_pelanggaran">
-                                        <option value='' selected>Pilih</option>
-                                        <option value="Rusak">Rusak</option>
-                                        <option value="Hilang">Hilang</option>
-                                        <option value="Hilang">Rusak-Telat</option>
-                                    </select>
-                                    <div class="text-danger">
-                                        @error('jenis_pelanggaran')
-                                        {{ $message }}
-                                        @enderror
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-md-2 col-form-label text-md-right">Jenis Denda</label>
-                                <div class="col-md-6">
-                                    <select class="custom-select" name="jenis_denda">
-                                        <option value='' selected>Pilih</option>
-                                        <option value="Ganti Koleksi yang Sama">Ganti Koleksi yang Sama</option>
-                                        <option value="Ganti Koleksi yang Baru">Ganti Koleksi yang Baru</option>
-                                        <option value="Ganti Uang">Ganti Uang</option>
-                                    </select>
-                                    <div class="text-danger">
-                                        @error('jenis_denda')
-                                        {{ $message }}
-                                        @enderror
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-md-2 col-form-label text-md-right">Denda</label>
-                                <div class="col-md-6">
-                                    <input type="text" class="form-control" name="jumlah_denda" value="{{ old('denda') }}" autocomplete="denda" autofocus>
-                                    <div class="text-danger">
-                                        @error('jumlah_denda')
-                                        {{ $message }}
-                                        @enderror
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <div class="col-md-8 text-md-right">
-                                    <button type="submit" class="btn btn-primary">Simpan</button>
-                                </div>
-                            </div>
-                        </form>
+  <div class="row justify-content-center">
+    <div class="col-md-12">
+      <div class="card card-primary card-outline">
+        <div class="card-body">
+          <div class="col-md-12">
+            <form action="{{url('/simpan-pelanggaran')}}" method="POST">
+              @csrf
+              <input type="text" name="no_transaksi" value="{{$pelanggaran->no_transaksi}}" hidden>
+              <input type="text" name="id_buku" value="{{$pelanggaran->id_buku}}" hidden>
+              <div class="form-group row">
+                <label class="col-md-2 col-form-label text-md-right">Jenis Pelanggaran</label>
+                <div class="col-md-6">
+                  <select class="custom-select" name="jenis_pelanggaran">
+                    <option value='' selected>Pilih</option>
+                    <option value="Rusak">Rusak</option>
+                    <option value="Hilang">Hilang</option>
+                    <option value="Telat">Telat</option>
+                  </select>
+                  <div class="text-danger">
+                      @error('jenis_pelanggaran')
+                      {{ $message }}
+                      @enderror
+                  </div>
+                </div>
+              </div>
+                <div class="form-group row">
+                    <label class="col-md-2 col-form-label text-md-right">Jenis Denda</label>
+                    <div class="col-md-6">
+                        <select class="custom-select" name="jenis_denda">
+                            <option value='' selected>Pilih</option>
+                            <option value="Ganti Koleksi yang Sama">Ganti Koleksi yang Sama</option>
+                            <option value="Ganti Koleksi yang Baru">Ganti Koleksi yang Baru</option>
+                            <option value="Ganti Uang">Ganti Uang</option>
+                        </select>
+                        <div class="text-danger">
+                            @error('jenis_denda')
+                            {{ $message }}
+                            @enderror
+                        </div>
                     </div>
                 </div>
-            </div>
+                <div class="form-group row">
+                    <label class="col-md-2 col-form-label text-md-right">Denda</label>
+                    <div class="col-md-6">
+                        <input type="text" class="form-control" name="jumlah_denda" value="{{ old('denda') }}" autocomplete="denda" autofocus>
+                        <div class="text-danger">
+                            @error('jumlah_denda')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-md-8 text-md-right">
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </div>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </div>
 <script>
     $(document).ready(function() {
